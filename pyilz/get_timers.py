99dd788dfe8a1a945bc7910fff8af5c251f3e69b
NickJordan289 <nickjordan289@gmail.com>
2023-06-01 23:03:22 +0800

    Fixed linting errors

--- pyilz/get_timers.py
+++ pyilz/get_timers.py
@@ -17,7 +17,8 @@ def get_timers(ca, aa, completed, init=False):
             cur_time = pd.Timestamp.now(tz='utc')
             diff_minutes = (end_time - cur_time).total_seconds() / 60
 
-            # Calculate percentage based on currentActivity.StartTime and currentActivity.EndTime
+            # Calculate percentage based on currentActivity.StartTime 
+            # and currentActivity.EndTime
             start_time = row['currentActivity.StartTime'].replace(
                 tzinfo=pytz.utc)
             total_time = (end_time - start_time).total_seconds() / 60
@@ -27,8 +28,12 @@ def get_timers(ca, aa, completed, init=False):
             notified = False
             if init and diff_minutes <= 0:
                 notified = True
-            timers[f'{row["uid"]}-current'] = {'uid': row['uid'], 'name': row['buildingTypeString'],
-                                               'type': row['currentActivity.Type'], 'minutes': diff_minutes, 'notified': notified, 'percentage': percentage}
+            timers[f'{row["uid"]}-current'] = {'uid': row['uid'], 
+                                               'name': row['buildingTypeString'],
+                                               'type': row['currentActivity.Type'], 
+                                               'minutes': diff_minutes, 
+                                               'notified': notified, 
+                                               'percentage': percentage}
 
     if aa is not None:
         for i, row in aa.iterrows():
@@ -36,21 +41,23 @@ def get_timers(ca, aa, completed, init=False):
             cur_time = pd.Timestamp.now(tz='utc')
             diff_minutes = (end_time - cur_time).total_seconds() / 60
 
-            # Calculate percentage based on currentActivity.StartTime and currentActivity.EndTime
+            # Calculate percentage based on currentActivity.StartTime 
+            # and currentActivity.EndTime
             start_time = row['autoActivity.StartTime'].replace(
                 tzinfo=pytz.utc)
             total_time = (end_time - start_time).total_seconds() / 60
             elapsed_time = (cur_time - start_time).total_seconds() / 60
             percentage = elapsed_time / total_time * 100
 
-            # if row['buildingTypeString'] in ['SEQUESTRIAN_PLANT_4', 'PHOTODISINTEGRATION_PLANT_4', 'CONDENSER_PLANT_4']:
-            #    percentage = (int(row['autoActivity.GeneratedResources']) / 7500) * 100
-
             notified = False
             if init and diff_minutes <= 0:
                 notified = True
-            timers[f'{row["uid"]}-auto'] = {'uid': row['uid'], 'name': row['buildingTypeString'],
-                                            'type': row['autoActivity.Type'], 'minutes': diff_minutes, 'notified': notified, 'percentage': percentage}
+            timers[f'{row["uid"]}-auto'] = {'uid': row['uid'], 
+                                            'name': row['buildingTypeString'],
+                                            'type': row['autoActivity.Type'], 
+                                            'minutes': diff_minutes, 
+                                            'notified': notified, 
+                                            'percentage': percentage}
 
     if completed is not None:
         for i, row in completed.iterrows():
@@ -59,21 +66,23 @@ def get_timers(ca, aa, completed, init=False):
             cur_time = pd.Timestamp.now(tz='utc')
             diff_minutes = (end_time - cur_time).total_seconds() / 60
 
-            # Calculate percentage based on currentActivity.StartTime and currentActivity.EndTime
+            # Calculate percentage based on currentActivity.StartTime 
+            # and currentActivity.EndTime
             start_time = row['completedActivity.StartTime'].replace(
                 tzinfo=pytz.utc)
             total_time = (end_time - start_time).total_seconds() / 60
             elapsed_time = (cur_time - start_time).total_seconds() / 60
             percentage = elapsed_time / total_time * 100
 
-            # if row['buildingTypeString'] in ['SEQUESTRIAN_PLANT_4', 'PHOTODISINTEGRATION_PLANT_4', 'CONDENSER_PLANT_4']:
-            #    percentage = (int(row['completedActivity.GeneratedResources']) / 7500) * 100
-
             notified = False
             if init and diff_minutes <= 0:
                 notified = True
-            timers[f'{row["uid"]}-current'] = {'uid': row['uid'], 'name': row['buildingTypeString'],
-                                               'type': row['completedActivity.Type'], 'minutes': diff_minutes, 'notified': notified, 'percentage': percentage}
+            timers[f'{row["uid"]}-current'] = {'uid': row['uid'], 
+                                               'name': row['buildingTypeString'],
+                                               'type': row['completedActivity.Type'], 
+                                               'minutes': diff_minutes, 
+                                               'notified': notified, 
+                                               'percentage': percentage}
 
     # sort by minutes
     timers = {k: v for k, v in sorted(
