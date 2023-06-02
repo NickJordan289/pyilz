140e9d0d28a4a6fe445c69a92690f3ed8afda0e5
NickJordan289 <nickjordan289@gmail.com>
2023-06-02 19:43:06 +0800

    Fixed test case

--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -1,15 +1,17 @@
 import json
 from pyilz.parse_land import parse_land
 from pyilz.get_timers import get_timers
+def test_e2e():
+    # Get list of plots
+    with open('./tests/example_game_state.json', 'r') as f:
+        plots = json.load(f)['data']
 
-# Get list of plots
-with open('./tests/example_game_state.json', 'r') as f:
-    plots = json.load(f)['data']
+    # Get Current, Automatic and Completed Actions
+    land = plots[2]['data']
+    _, _, ca, aa, completed = parse_land(land)
 
-# Get Current, Automatic and Completed Actions
-land = plots[2]['data']
-_, _, ca, aa, completed = parse_land(land)
+    # Get timers from actions
+    timers = get_timers(ca, aa, completed)
 
-# Get timers from actions
-timers = get_timers(ca, aa, completed)
-print(timers)
+    # If no exception, test passed
+    assert True
