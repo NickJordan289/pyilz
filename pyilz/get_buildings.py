4987c26376a605831a4ed1730f05db69d3fc1323
Nick Jordan <nickjordan289@gmail.com>
2023-06-06 01:03:16 +0800

    Updated fuel extractors and converters and -25% to all tier 2 numbers

--- pyilz/get_buildings.py
+++ pyilz/get_buildings.py
@@ -3,8 +3,11 @@ import math
 with open('./pyilz/buildings.json', 'r') as f:
     buildings = json.load(f)['buildings']
 
-def list_buildings():
+def find_missing_activities():
+    IGNORE_LIST = ['Nexus','Engineering Workshop','Quantum Fabricant','Power Station','Carbon Matter Silo','Silicon Matter Silo','Hydrogen Matter Silo','Crypton Containment Unit','Crypton Containment Unit','Solon Containment Unit','Zero Point Transducer','Data Bank','Holographic Statue','Marketplace']
     for building in buildings:
+        if building['name'] in IGNORE_LIST:
+            continue
         details = building['details']
         for detail in details:
             level = detail['level']
@@ -14,17 +17,34 @@ def list_buildings():
             activities = detail['activities'] if 'activities' in detail else {}
             if 'passive' not in activities:
                 print(building['name'],level,'NO PASSIVE')
+            else:
+                passive = activities['passive']
+                if passive:
+                    amount = passive['amount']
+                    if not amount:
+                        print(building['name'],level,'NO PASSIVE')
+                else:
+                    print(building['name'],level,'NO PASSIVE')
             if 'active' not in activities:
                 print(building['name'],level,'NO ACTIVE')
+            else:
+                active = activities['active']
+                if active:
+                    amount = active['amount']
+                    if not amount:
+                        print(building['name'],level,'NO ACTIVE')
+                else:
+                    print(building['name'],level,'NO ACTIVE')
             cost = detail['cost']
             for c in cost:
                 resource = c['resource']
                 amount = c['amount']
 
 def get_active_output(name, level, tier, efficiency=100):
+    name = name.lower().replace(' ','').replace('_','')
     land_tier_bonus_multiplier_percent = [0,0,33.33333333333,100,300,900]
-    for building in buildings:
-        if building['name'] == name:
+    for building in buildings: # This should probably be a dict
+        if building['nameId'] == name:
             details = building['details']
             for detail in details:
                 if detail['level'] == level:
@@ -34,8 +54,7 @@ def get_active_output(name, level, tier, efficiency=100):
                     return math.ceil((amount * efficiency / 100) * (1 + (land_tier_bonus_multiplier_percent[tier] / 100)))
 
 if __name__ == '__main__':
-    #list_buildings()
-    hydrogen_pump = get_active_output('Hydrogen Pump', 5, 2, 150)
-    seq = get_active_output('Sequestrian Plant', 6, 2, 150)
-    
-    import code; code.interact(local=dict(globals(), **locals()))
+    find_missing_activities()
+    #hydrogen_pump = get_active_output('Hydrogen Pump', 5, 2, 150)
+    #seq = get_active_output('Sequestrian Plant', 6, 2, 150)
+    #import code; code.interact(local=dict(globals(), **locals()))
