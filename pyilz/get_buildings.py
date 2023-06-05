fe983a852331c6557cd0e5a6bbab2cf99fede0eb
NickJordan289 <nickjordan289@gmail.com>
2023-06-05 23:28:10 +0800

    Updated 1-7 for carbon/silicon/hydrogen buildinggs

--- pyilz/get_buildings.py
+++ pyilz/get_buildings.py
@@ -1,4 +1,5 @@
 import json
+import math
 with open('./pyilz/buildings.json', 'r') as f:
     buildings = json.load(f)['buildings']
 
@@ -20,6 +21,21 @@ def list_buildings():
                 resource = c['resource']
                 amount = c['amount']
 
+def get_active_output(name, level, tier, efficiency=100):
+    land_tier_bonus_multiplier_percent = [0,0,33.33333333333,100,300,900]
+    for building in buildings:
+        if building['name'] == name:
+            details = building['details']
+            for detail in details:
+                if detail['level'] == level:
+                    activities = detail['activities']
+                    active = activities['active']
+                    amount = int(active['amount'])                    
+                    return math.ceil((amount * efficiency / 100) * (1 + (land_tier_bonus_multiplier_percent[tier] / 100)))
+
 if __name__ == '__main__':
-    list_buildings()
-#import code; code.interact(local=dict(globals(), **locals()))
+    #list_buildings()
+    hydrogen_pump = get_active_output('Hydrogen Pump', 5, 2, 150)
+    seq = get_active_output('Sequestrian Plant', 6, 2, 150)
+    
+    import code; code.interact(local=dict(globals(), **locals()))
